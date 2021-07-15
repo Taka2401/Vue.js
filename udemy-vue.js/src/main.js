39cf226f8cc831e1c5d29b467e2f0901bc8df0a5
Taka2401 <tkht2401@gmail.com>
2021-07-15 18:57:53 +0900

    add: カスタム フック関数

--- udemy-vue.js/src/main.js
+++ udemy-vue.js/src/main.js
@@ -4,7 +4,25 @@ import LikeNumber from "./components/LikeNumber.vue"
 
 Vue.config.productionTip = false
 Vue.component('LikeNumber', LikeNumber);
+Vue.directive('border', {
+  bind() {
+    // ディレクティブが初めて対象の要素に紐づいたとき
+  },
+  inserted() {
+    // 親Nodeに挿入されたとき
+  },
+  update() {
+    // コンポーネントが更新された度。子コンポーネントが更新される前
+  },
+  componentUpdated() {
+    // コンポーネントが更新された度。子コンポーネントが更新される後
+  },
+  unbind() {
+    // ディレクティブが紐づいてる要素から取り除かれた時
+  },
+});
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
