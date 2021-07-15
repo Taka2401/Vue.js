0624f2bf1da2d30e02b191367a8f82d78195fcf4
Taka2401 <tkht2401@gmail.com>
2021-07-15 17:13:46 +0900

    add: 双方向バインディング

--- udemy-vue.js/src/App.vue
+++ udemy-vue.js/src/App.vue
@@ -17,11 +17,40 @@
 
       <label for="maxNumber">最大人数</label>
       <input id="maxNumber" type="number" v-model.number="eventData.maxNumber">
-      <p>{{ typeof eventData.maxNumber}}</p>
+      <p>{{ eventData.maxNumber}}</p>
 
       <label for="host">主催者</label>
       <input id="host" type="text" v-model.trim="eventData.host">
       <pre>{{ eventData.host }}</pre>
+
+      <label for="detail">イベントの内容</label>
+      <textarea id="detail" cols="30" rows="10" v-model="eventData.detail"></textarea>
+      <pre>{{ eventData.detail }}</pre>
+
+
+      <input type="checkbox" id="isPrivate" v-model="eventData.isPrivate">
+      <label for="isPrivate">非公開</label>
+      <p>{{ eventData.isPrivate }}</p>
+
+      <p>参加条件</p>
+      <input type="checkbox" id="10" value="10代" v-model="eventData.target">
+      <label for="10">10代</label>
+      <input type="checkbox" id="20" value="20代" v-model="eventData.target">
+      <label for="20">20代</label>
+      <input type="checkbox" id="30" value="30代" v-model="eventData.target">
+      <label for="30">30代</label>
+      <p>{{ eventData.target }}</p>
+
+      <p>参加費</p>
+      <input type="radio" id="free" value="無料" v-model="eventData.price">
+      <label for="free">無料</label>
+      <input type="radio" id="paid" value="有料" v-model="eventData.price">
+      <label for="paid">有料</label>
+
+      <p>開催場所</p>
+      <select>
+        <option>東京</option>
+      </select>
     </div>
   </div>
 </template>
@@ -37,9 +66,13 @@ export default {
       number: 18,
       currentComponent: "Home",
       eventData: {
-        title: "タイトル",
+        title: "",
         maxNumber: 0,
-        host: ""
+        host: "",
+        detail: "",
+        isPrivate: false,
+        target: [],
+        price: "無料"
       }
     };
   },
