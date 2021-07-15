8e3a4ff33bee74161ee3d8fcff234428469b4901
Taka2401 <tkht2401@gmail.com>
2021-07-15 12:40:31 +0900

    add: 名前付きslot

--- udemy-vue.js/src/components/LikeHeader.vue
+++ udemy-vue.js/src/components/LikeHeader.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
-    <slot></slot>
+    <slot name="title"></slot>
+    <hr>
+    <p>いいねの数</p>
+    <slot name="number"></slot>
   </div>
 </template>
 
 <script>
 export default {
   props: ["headerText"]
-}
+};
 </script>
+
+<style>
+</style>
