d8ba9ac6f3be458804799c258fca183451c441dd
Taka2401 <tkht2401@gmail.com>
2021-07-17 17:24:11 +0900

    add: trantionFade

--- udemy-vue.js/src/App.vue
+++ udemy-vue.js/src/App.vue
@@ -1,96 +1,68 @@
+
 <template>
-  <div>
-    <LikeHeader>
-      <p>はじめまして</p>
-    </LikeHeader>
-    <LikeNumber :total-number="number" @my-click="incrementNumber"></LikeNumber>
-    <button @click="currentComponent = 'Home'">Home</button>
-    <button @click="currentComponent = 'About'">About</button>
-    <keep-alive>
-      <component :is="currentComponent"></component>
-    </keep-alive>
-    <div style="padding: 10rem;">
-      <h2>イベントのフォーム</h2>
-
-      <EventTitle v-model="eventData.title"></EventTitle>
-
-      <label for="maxNumber">最大人数</label>
-      <input id="maxNumber" type="number" v-model.number="eventData.maxNumber">
-      <p>{{ eventData.maxNumber}}</p>
-
-      <label for="host">主催者</label>
-      <input id="host" type="text" v-model.trim="eventData.host">
-      <pre>{{ eventData.host }}</pre>
-
-      <label for="detail">イベントの内容</label>
-      <textarea id="detail" cols="30" rows="10" v-model="eventData.detail"></textarea>
-      <pre>{{ eventData.detail }}</pre>
-
-
-      <input type="checkbox" id="isPrivate" v-model="eventData.isPrivate">
-      <label for="isPrivate">非公開</label>
-      <p>{{ eventData.isPrivate }}</p>
-
-      <p>参加条件</p>
-      <input type="checkbox" id="10" value="10代" v-model="eventData.target">
-      <label for="10">10代</label>
-      <input type="checkbox" id="20" value="20代" v-model="eventData.target">
-      <label for="20">20代</label>
-      <input type="checkbox" id="30" value="30代" v-model="eventData.target">
-      <label for="30">30代</label>
-      <p>{{ eventData.target }}</p>
-
-      <p>参加費</p>
-      <input type="radio" id="free" value="無料" v-model="eventData.price">
-      <label for="free">無料</label>
-      <input type="radio" id="paid" value="有料" v-model="eventData.price">
-      <label for="paid">有料</label>
-
-      <p>開催場所</p>
-      <select v-model="eventData.location" multiple>
-        <option v-for="location in locations" :key="location">
-          {{ location }}
-        </option>
-      </select>
-      <p>{{ eventData.location }}</p>
-    </div>
+  <div class="main">
+    <button @click="show = !show">切り替え</button>
+    <transition name="fade">
+      <p v-if="show">hello</p>
+    </transition>
+    <transition name="slide">
+      <p v-if="show">bye</p>
+    </transition>
   </div>
 </template>
 
 <script>
-import LikeHeader from "./components/LikeHeader.vue";
-import Home from "./components/Home.vue";
-import About from "./components/About.vue";
-import EventTitle from "./components/EventTitle.vue";
-
 export default {
   data() {
     return {
-      number: 18,
-      currentComponent: "Home",
-      locations: ["東京", "名古屋", "大阪"],
-      eventData: {
-        title: "",
-        maxNumber: 0,
-        host: "",
-        detail: "",
-        isPrivate: false,
-        target: [],
-        price: "無料",
-        location: []
-      }
+      show: true
     };
-  },
-  components: {
-    LikeHeader,
-    Home,
-    About,
-    EventTitle
-  },
-  methods: {
-    incrementNumber(value) {
-      this.number = value;
-    }
   }
 };
 </script>
+
+<style scoped>
+.fade-enter {
+  opacity: 0;
+}
+.fade-enter-active {
+  transition: opacity 0.5s;
+}
+.fade-enter-to {
+  opacity: 1;
+}
+.fade-leave {
+  opacity: 1;
+}
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-leave-to {
+  opacity: 0;
+}
+
+.slide-enter-active {
+  animation: slide-in 0.5s;
+}
+
+.slide-leave-active {
+  animation: slide-in 0.5s reverse;
+}
+
+
+@keyframes slide-in {
+  from  {
+    transform: translatex(100px);
+  }
+  to {
+    transform: translatex(0);
+  }
+}
+
+.main {
+  width: 70%;
+  margin: auto;
+  padding-top: 5rem;
+  text-align: center;
+}
+</style>
