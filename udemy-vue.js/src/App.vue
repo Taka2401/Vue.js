e14ff6667f6b1d030da4a9687220c29d6b0b4404
Taka2401 <tkht2401@gmail.com>
2021-07-17 18:00:08 +0900

    add: animate

--- udemy-vue.js/src/App.vue
+++ udemy-vue.js/src/App.vue
@@ -2,10 +2,16 @@
 <template>
   <div class="main">
     <button @click="show = !show">切り替え</button>
-    <transition name="fade">
+    <transition
+      enter-active-class="animate__animated animate__bounce"
+      leave-active-class="animate__animated animate__rubberBand"
+      >
       <p v-if="show">hello</p>
     </transition>
-    <transition name="slide">
+    <transition 
+      :name="slide" 
+      type="animation" appear
+      >
       <p v-if="show">bye</p>
     </transition>
   </div>
@@ -41,12 +47,19 @@ export default {
   opacity: 0;
 }
 
+.slide-enter,
+.slide-leave-to {
+  opacity: 0;
+}
+
 .slide-enter-active {
   animation: slide-in 0.5s;
+  transition: opacity 5s;
 }
 
 .slide-leave-active {
   animation: slide-in 0.5s reverse;
+  transition: opacity 5s;
 }
 
 
