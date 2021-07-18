f4f1f73cc823615cac890a57975355186067fc36
Taka2401 <tkht2401@gmail.com>
2021-07-18 13:14:12 +0900

    activeClass

--- udemy-vue.js/src/App.vue
+++ udemy-vue.js/src/App.vue
@@ -1,5 +1,28 @@
 <template>
-  <div>
+  <div style="margin: 100px auto; text-align: center;">
+    <nav>
+      <router-link
+        to="/"
+        active-class="active--link"
+        exact
+        class="link"
+      >home</router-link>
+      <router-link
+        to="users"
+        active-class="active--link"
+        exact
+        class="link"
+        >users</router-link>
+    </nav>
     <router-view></router-view>
   </div>
 </template>
+
+<style scoped>
+.link {
+  margin: 10px;
+}
+.active--link {
+  font-size: 30px;
+}
+</style>
