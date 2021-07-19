fdd170520ed5e6415b260572e1fb8f0f7f728230
Taka2401 <tkht2401@gmail.com>
2021-07-19 12:33:54 +0900

    axios

--- udemy-vue.js/src/App.vue
+++ udemy-vue.js/src/App.vue
@@ -1,20 +1,69 @@
 <template>
-  <div style="width: 700px; margin: 100px auto; text-align: center;">
-    <router-view name="header"></router-view>
-    <transition name="fade" mode="out-in">
-      <router-view></router-view>
-    </transition>
+  <div>
+    <h3>掲示板に投稿する</h3>
+    <label for="name">ニックネーム</label>
+    <input id="name" type="text" v-model="name">
+
+    <br><br>
+    <label for="comment">コメント</label>
+    <textarea id="comment" v-model="comment"></textarea>
+    <br><br>
+
+    <button @click="createComment">サーバーに送信</button>
+
+    <h2>掲示板</h2>
+    <div v-for="post in posts" :key="post.name">
+      <div>名前： {{ post.fields.name.stringValue }}</div>
+      <div>コメント： {{ post.fields.comment.stringValue }}</div>
+      <br>
+    </div>
   </div>
 </template>
 
-<style scoped>
-.fade-enter,
-.fade-leave-to {
-  opacity: 0;
-}
+<script>
+import axios from "axios";
 
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.5s;
+export default {
+  data() {
+    return {
+      name: "",
+      comment: "",
+      posts: []
+    };
+  },
+  created() {
+    axios.get('https://firestore.googleapis.com/v1/projects/my-project-34927/databases/(default)/documents/comments',
+    )
+    .then(response => {
+      this.posts = response.data.documents;
+    });
+  },
+  methods: {
+    createComment() {
+      axios.post(
+        'https://firestore.googleapis.com/v1/projects/my-project-34927/databases/(default)/documents/comments',
+        {
+          fields: {
+            name: {
+              stringValue: this.name
+            },
+            comment: {
+              stringValue: this.comment
+            }
+          }
+        }
+      );
+      this.name = "";
+      this.commment = "";
+    }
+  }
+};
+</script>
+
+<style scoped>
+div {
+  width: 700px;
+  margin: 10px auto;
+  text-align: center;
 }
 </style>
